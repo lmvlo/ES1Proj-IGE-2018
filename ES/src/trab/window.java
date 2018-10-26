a8443ab579f7e52ee93b52cbf19a57621d15d01c
diogonunosarmento <dnsta@iscte-iul.pt>
2018-10-26 04:18:02 +0100

    d

--- ES/src/trab/window.java
+++ ES/src/trab/window.java
@@ -7,17 +7,26 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JCheckBox;
 
 public class window {
 
-	private JFrame frame;
+	private JFrame frmBda;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
 
 	public static void NewScreen() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					window window = new window();
-					window.frame.setVisible(true);
+					window.frmBda.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -32,46 +41,129 @@ public class window {
 
 	
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 1003, 708);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmBda = new JFrame();
+		frmBda.setTitle("BDA");
+		frmBda.setBounds(100, 100, 1003, 708);
+		frmBda.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JButton btnNewButton = new JButton("New button");
+		JButton btnNewButton_3 = new JButton("Actualizar");
 		
-		JButton btnNewButton_1 = new JButton("New button");
+		JLabel lblNewLabel = new JLabel("Procurar por Autor:");
 		
-		JButton btnNewButton_2 = new JButton("New button");
+		textField = new JTextField();
+		textField.setColumns(10);
 		
-		JButton btnNewButton_3 = new JButton("Update");
-		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		textField_1 = new JTextField();   
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Procurar por palavra-chave:");
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		
+		JButton btnNewButton_4 = new JButton("Enviar");
+		
+		JTextPane textPane = new JTextPane();
+		
+		JCheckBox chckbxNewCheckBox = new JCheckBox("Facebook");
+		
+		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("Email");
+		
+		JCheckBox chckbxNewCheckBox_2 = new JCheckBox("Twitter");
+		
+		JLabel lblNewLabel_2 = new JLabel("De:");
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Data de publica\u00E7\u00E3o");
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Email");
+		GroupLayout groupLayout = new GroupLayout(frmBda.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(170)
+					.addGap(24)
+					.addComponent(btnNewButton_3, GroupLayout.PREFERRED_SIZE, 237, GroupLayout.PREFERRED_SIZE)
+					.addGap(90)
 					.addComponent(btnNewButton)
-					.addGap(94)
-					.addComponent(btnNewButton_1)
-					.addPreferredGap(ComponentPlacement.RELATED, 196, Short.MAX_VALUE)
-					.addComponent(btnNewButton_2)
-					.addGap(176))
+					.addGap(18)
+					.addComponent(lblNewLabel_2)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(37)
+					.addComponent(lblNewLabel_3)
+					.addGap(18)
+					.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(94, Short.MAX_VALUE))
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(411)
-					.addComponent(btnNewButton_3)
-					.addContainerGap(455, Short.MAX_VALUE))
+					.addGap(41)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(chckbxNewCheckBox_2)
+							.addContainerGap())
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+							.addGroup(groupLayout.createSequentialGroup()
+								.addComponent(chckbxNewCheckBox_1)
+								.addContainerGap())
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(chckbxNewCheckBox)
+									.addContainerGap())
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 301, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblNewLabel)
+										.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 208, GroupLayout.PREFERRED_SIZE)
+										.addComponent(textField, GroupLayout.PREFERRED_SIZE, 207, GroupLayout.PREFERRED_SIZE))
+									.addPreferredGap(ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addGroup(groupLayout.createSequentialGroup()
+											.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, 458, GroupLayout.PREFERRED_SIZE)
+											.addPreferredGap(ComponentPlacement.RELATED)
+											.addComponent(btnNewButton_4))
+										.addComponent(textPane, GroupLayout.PREFERRED_SIZE, 460, GroupLayout.PREFERRED_SIZE))
+									.addGap(7))))))
 		);
 		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
-					.addGap(88)
-					.addComponent(btnNewButton_3)
-					.addPreferredGap(ComponentPlacement.RELATED, 381, Short.MAX_VALUE)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnNewButton)
-						.addComponent(btnNewButton_1)
-						.addComponent(btnNewButton_2))
-					.addGap(125))
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+					.addGap(27)
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+							.addComponent(lblNewLabel_2)
+							.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addComponent(lblNewLabel_3)
+							.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
+						.addComponent(btnNewButton_3, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE))
+					.addGap(36)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addComponent(lblNewLabel)
+							.addGap(37)
+							.addComponent(textField, GroupLayout.PREFERRED_SIZE, 43, GroupLayout.PREFERRED_SIZE)
+							.addGap(70)
+							.addComponent(lblNewLabel_1))
+						.addComponent(textPane, Alignment.TRAILING, GroupLayout.PREFERRED_SIZE, 183, GroupLayout.PREFERRED_SIZE))
+					.addGap(31)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 41, GroupLayout.PREFERRED_SIZE)
+							.addGap(67)
+							.addComponent(chckbxNewCheckBox))
+						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+							.addComponent(btnNewButton_4)
+							.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, 101, GroupLayout.PREFERRED_SIZE)))
+					.addGap(32)
+					.addComponent(chckbxNewCheckBox_1)
+					.addGap(27)
+					.addComponent(chckbxNewCheckBox_2)
+					.addGap(28))
 		);
-		frame.getContentPane().setLayout(groupLayout);
+		frmBda.getContentPane().setLayout(groupLayout);
 	}
-
 }
